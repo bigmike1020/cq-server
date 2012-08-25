5abdc3a72e3029a48c31b855f57c12a80addbe08
eternalmatt <eternalmatt@gmail.com>
2012-08-25 10:51:23 -0400

    Basic layout for take picture activity

--- app/src/com/icm/MainActivity.java
+++ app/src/com/icm/MainActivity.java
@@ -3,10 +3,10 @@ package com.icm;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
 public class MainActivity extends SherlockActivity {
 
@@ -15,22 +15,24 @@ public class MainActivity extends SherlockActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        final Context context = this;
-        
-        findViewById(R.id.button1).setOnClickListener(
-    		new View.OnClickListener() {
-				public void onClick(View v) {
-
-			        Intent intent = new Intent();
-			        intent.setClass(context, TakePictureActivity.class);
-			        startActivity(intent);
-					
-				}
-		});
     }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
+		final Context context = this;
+        
+		MenuItem item = menu.add("New");
+		item.setIcon(R.drawable.ic_menu_camera);
+		item.setShowAsAction(MenuItem.SHOW_AS_ACTION_WITH_TEXT | MenuItem.SHOW_AS_ACTION_ALWAYS);
+		//item.setTitleCondensed("New");
+		item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+			public boolean onMenuItemClick(MenuItem item) {
+				Intent intent = new Intent();
+		        intent.setClass(context, TakePictureActivity.class);
+		        startActivity(intent);
+				return true;
+			}
+		});
 		
 		return super.onCreateOptionsMenu(menu);
 	}
--- app/src/com/icm/TakePictureActivity.java
+++ app/src/com/icm/TakePictureActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
 public class TakePictureActivity extends SherlockActivity {
 	
@@ -11,11 +12,24 @@ public class TakePictureActivity extends SherlockActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_takepicture);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
     }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		
 		return super.onCreateOptionsMenu(menu);
+	}
+	
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			onBackPressed();
+		}
+		
+		return true;
 	}
 }
