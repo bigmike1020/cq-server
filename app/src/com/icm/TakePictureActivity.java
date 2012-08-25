c5eee7db4b36381db784fc9c2b31d93bc9319ca9
OzoneTank <wates.cole@gmail.com>
2012-08-25 10:59:11 -0400

    some initial work to camera

--- app/src/com/icm/TakePictureActivity.java
+++ app/src/com/icm/TakePictureActivity.java
@@ -1,21 +1,57 @@
 package com.icm;
 
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 public class TakePictureActivity extends SherlockActivity {
+	private static final int CAMERA_REQUEST = 1000;
+	private static final int RESULT_LOAD_IMAGE = 1001;
+	private ImageView imageView;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_takepicture);
-    }
+        this.imageView = (ImageView)this.findViewById(R.id.imageView1);
+        Button cameraButton = (Button) this.findViewById(R.id.button1);
+        cameraButton.setOnClickListener(new View.OnClickListener(){
+			
+			@Override
+			public void onClick(View v) {
+				Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+				startActivityForResult(cameraIntent, CAMERA_REQUEST);
+			}
+		});
+        Button photoGalleryButton = (Button) this.findViewById(R.id.button2);
+        photoGalleryButton.setOnClickListener(new View.OnClickListener(){
+			
+			@Override
+			public void onClick(View v) {
+				Intent cameraIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+				startActivityForResult(cameraIntent, RESULT_LOAD_IMAGE);
+			}
+		});
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		
 		return super.onCreateOptionsMenu(menu);
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(requestCode == CAMERA_REQUEST) {
+			Bitmap photo = (Bitmap) data.getExtras().get("data");
+			imageView.setImageBitmap(photo);
+		}
 	}
 }
