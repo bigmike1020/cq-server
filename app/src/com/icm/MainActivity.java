96e861ff501958bb693d8d4411b270201c8d03a8
eternalmatt <eternalmatt@gmail.com>
2012-08-25 12:14:10 -0400

    Changed MainActivity to extent a ListActivity instead

--- app/src/com/icm/MainActivity.java
+++ app/src/com/icm/MainActivity.java
@@ -1,21 +1,58 @@
 package com.icm;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
 
-import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockListActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 
-public class MainActivity extends SherlockActivity {
+public class MainActivity extends SherlockListActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        final Activity context = this;
+        
+        String array[] = { "Hello, World", "What's", "Up", "Ya'll" };
+        ListAdapter adapter = new ArrayAdapter<String>(this, R.layout.table_main_row, array){
+        	
+        	@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				View row = convertView;
+				if (convertView == null) {
+					LayoutInflater inflater = context.getLayoutInflater();
+					row = inflater.inflate(R.layout.table_main_row, null);
+					
+					ImageView imageView = (ImageView) row.findViewById(R.id.row_imageView);
+					imageView.setImageResource(R.drawable.ic_menu_camera);
+				
+					TextView textView = (TextView) row.findViewById(R.id.row_textView);
+					textView.setText("Row " + position + " -- " + getItem(position));
+					
+				}
+				return row;
+				
+			}
+        	
+        };
+        
+        
+        setListAdapter(adapter);
         
     }
+    
+    
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
